ea21c24adf67f25b783a83a3817e391327151b8e
Adrian Ollero <adrian.ollero@alu.uclm.es>
2019-01-11 00:46:23 +0100

    Added HUD class and HUD Widget

---
+++ Source/Puzzle/BattleShipHUD.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "BattleShipHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLE_API ABattleShipHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	ABattleShipHUD();
+	
+	
+};
